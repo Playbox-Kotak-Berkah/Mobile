c80d2da24f3489ac805deffff772399adc879e50
mfadlihs <winnerfadli999@gmail.com>
2023-07-02 07:20:01 +0700

    feat: color options

--- lib/app/view/onboarding_screen.dart
+++ lib/app/view/onboarding_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:playbox/app/controller/onboarding_controller.dart';
+import 'package:playbox/utils/color_constants.dart';
+import 'package:sizer/sizer.dart';
 
 class OnboardingScreen extends GetView<OnboardingController> {
   const OnboardingScreen({super.key});
@@ -8,7 +10,48 @@ class OnboardingScreen extends GetView<OnboardingController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // TODO: create this page
-        );
+      body: SizedBox(
+        width: 100.w,
+        height: 100.h,
+        child: Stack(
+          children: [
+            Image.asset(
+              "assets/images/onboarding_1.png",
+              width: 100.w,
+              height: 75.h,
+              fit: BoxFit.fill,
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: 100.w,
+                height: 45.h,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: ColorConstants.slate[25],
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                  boxShadow: [
+                    ColorConstants.shadow[1]!,
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [Text('Kelola Tambak dalam Satu Genggaman.')],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/utils/color_constants.dart
+++ lib/utils/color_constants.dart
@@ -16,4 +16,23 @@ class ColorConstants {
   };
 
   // NOTE: currently UI doesn't have text style guides, so i made it by myself. Change it if text styles guide availables
+  static const shadow = {
+    1: BoxShadow(
+      color: Color.fromRGBO(0, 0, 0, 0.04),
+      blurRadius: 10,
+      spreadRadius: 10,
+    ),
+  };
+
+  static const primary = {
+    100: Color(0xffCFE0F4),
+    200: Color(0xffA0C2E9),
+    300: Color(0xff70A3DE),
+    400: Color(0xff4185D3),
+    500: Color(0xff1166C8),
+    600: Color(0xff0E52A0),
+    700: Color(0xff0A3D78),
+    800: Color(0xff072950),
+    900: Color(0xff031428),
+  };
 }
